6f10603249f478e169bd3e7c98bec99724e39dde
Olivia Bonnal <oliviabonnal@gmail.com>
2022-10-04 19:06:02 +0100

    added associations and validations between users and flats and updated the seed

--- app/models/flat.rb
+++ app/models/flat.rb
@@ -1,5 +1,7 @@
 class Flat < ApplicationRecord
-  validates :name, :address, :description, :price_per_night, :number_of_guests, presence: true
+  belongs_to :user
+
+  validates :name, :address, :description, :price_per_night, :number_of_guests, :user_id, presence: true
   validates :description, length: { minimum: 10, maximum: 400 }
   validates :number_of_guests, numericality: { greater_than: 0 }
   validates :price_per_night, numericality: { greater_than_or_equal_to: 0 }
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,13 +5,40 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+puts "Destroying all users and flats..."
+User.destroy_all
 puts "Destroying all flats..."
 Flat.destroy_all
 
 puts "Creating new users..."
-User.create!(
 
+robert = User.create!(
+  name: "Robert",
+  email: "robert@mail.com",
+  password: "ilikeflats"
 )
+puts "User 1 created"
+
+linda = User.create!(
+  name: "Linda",
+  email: "linda@mail.com",
+  password: "ilikeflats2"
+)
+puts "User 2 created"
+
+mel = User.create!(
+  name: "Mel",
+  email: "mel@mail.com",
+  password: "ilikeflats3"
+)
+puts "User 3 created"
+
+josh = User.create!(
+  name: "Josh",
+  email: "josh@mail.com",
+  password: "ilikeflats4"
+)
+puts "User 4 created"
 
 puts "Creating new flats..."
 
@@ -20,7 +47,8 @@ Flat.create!(
   address: '10 Clifton Gardens, London W9 1DT',
   description: 'A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
   price_per_night: 90,
-  number_of_guests: 4
+  number_of_guests: 4,
+  user: robert
 )
 puts "Flat 1 created"
 
@@ -29,7 +57,8 @@ Flat.create!(
   address: '236 Baker St, London NW1 5RS',
   description: 'A quaint room in the heart of London. One double bedroom with view on the Sherlock Holmes Museum',
   price_per_night: 85,
-  number_of_guests: 2
+  number_of_guests: 2,
+  user: linda
 )
 puts "Flat 2 created"
 
@@ -38,7 +67,8 @@ Flat.create!(
   address: '236 Baker St, London NW1 5RS',
   description: 'A quaint room in the heart of London. One double bedroom with view on the Sherlock Holmes Museum.',
   price_per_night: 75,
-  number_of_guests: 2
+  number_of_guests: 2,
+  user: mel
 )
 puts "Flat 3 created"
 
@@ -47,7 +77,8 @@ Flat.create!(
   address: '4 Sussex Place, London W2 2TP',
   description: 'Modern studio with all necessary amenities, just an 8-minute walk from Paddington station and Hyde Park.',
   price_per_night: 100,
-  number_of_guests: 3
+  number_of_guests: 3,
+  user: linda
 )
 puts "Flat 4 created"
 
@@ -56,7 +87,8 @@ Flat.create!(
   address: '101 Bayham St, London NW1 0AG',
   description: 'A quiet flat close to the bustling market of Camden. En-suite bathroom and large kitchen with all modern amenities.',
   price_per_night: 65,
-  number_of_guests: 2
+  number_of_guests: 2,
+  user: robert
 )
 puts "Flat 5 created"
 
@@ -65,7 +97,8 @@ Flat.create!(
   address: '135 Petersham road, Richmond TW10 7AA',
   description: '17th century manor house opposite Richmond Park. Huge garden with swimming pool, 5 double bedrooms, en-suite bathrooms and a modern kitchen.',
   price_per_night: 2000,
-  number_of_guests: 7
+  number_of_guests: 7,
+  user: josh
 )
 puts "Flat 6 created"
 puts "All done!"
