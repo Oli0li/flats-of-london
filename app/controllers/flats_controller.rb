e7372e0c263748c92add34e7c3c9cdd3b33a69b9
Olivia Bonnal <oliviabonnal@gmail.com>
2022-10-04 19:46:54 +0100

    added update and destroy methods for flats, only visible for the creator

--- app/controllers/flats_controller.rb
+++ app/controllers/flats_controller.rb
@@ -1,6 +1,6 @@
 class FlatsController < ApplicationController
   skip_before_action :authenticate_user!, only: [:index, :show]
-  before_action :set_flat, only: [:show]
+  before_action :set_flat, only: [:show, :update, :destroy]
 
   def index
     @flats = Flat.all
@@ -15,11 +15,21 @@ class FlatsController < ApplicationController
 
   def create
     @flat = Flat.new(flat_params)
+    @flat.user = current_user
     if @flat.save
       redirect_to flat_path(@flat)
     else
       render :new, status: :unprocessable_entity
     end
+  end
+
+  def update
+    @flat.update(flat_params)
+  end
+
+  def destroy
+    @flat.destroy
+    redirect_to flats_path, status: :see_other
   end
 
   private
