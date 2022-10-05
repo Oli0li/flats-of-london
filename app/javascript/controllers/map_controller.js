2a8f31a538db08f798f886af419cec2f691290af
Olivia Bonnal <oliviabonnal@gmail.com>
2022-10-05 12:54:46 +0100

    added map with markers

---
+++ app/javascript/controllers/map_controller.js
@@ -0,0 +1,36 @@
+import { Controller } from "@hotwired/stimulus"
+
+// Connects to data-controller="map"
+export default class extends Controller {
+  static values = {
+    apiKey: String,
+    markers: Array
+  }
+
+  connect() {
+    // Authentify with MapBox API
+    mapboxgl.accessToken = this.apiKeyValue;
+    // Use MapBox in div
+    this.map = new mapboxgl.Map({
+      container: this.element,
+      style: "mapbox://styles/mapbox/streets-v10"
+    });
+    this.#addMarkersToMap();
+    this.#fitMapToMarkers();
+  }
+
+  // Define a private method to add markers to the map
+  #addMarkersToMap() {
+    this.markersValue.forEach((marker) => {
+      new mapboxgl.Marker()
+      .setLngLat([marker.lng, marker.lat])
+      .addTo(this.map)
+    });
+  }
+
+  #fitMapToMarkers() {
+    const bounds = new mapboxgl.LngLatBounds();
+    this.markersValue.forEach(marker => bounds.extend([marker.lng, marker.lat]));
+    this.map.fitBounds(bounds, { padding: 70, maxZoom: 15, duration: 0 })
+  }
+}
