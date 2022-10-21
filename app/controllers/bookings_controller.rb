349217c8897751be3c0ee1c52e71441c358f0207
Olivia Bonnal <oliviabonnal@gmail.com>
2022-10-21 15:44:05 +0100

    removed redundant comments

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,12 +1,11 @@
 class BookingsController < ApplicationController
   def create
     @flat = Flat.find(params[:flat_id])
-    # @booking = Booking.new(user: current_user, flat: @flat, start_date: params[:start_date], end_date: params[:end_date])
-    # @booking = Booking.new
     @booking = Booking.new(booking_params)
     @booking.user = current_user
     @booking.flat = @flat
     if @booking.save
+      # Will change this to dashboard later
       redirect_to root_path
     else
       render "flats/show", status: :unprocessable_entity
