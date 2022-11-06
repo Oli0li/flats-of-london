274f0bce148b46ebd9e735a91537f0f2dea00b12
Olivia Bonnal <oliviabonnal@gmail.com>
2022-11-06 18:43:30 +0000

    refactored flatpickr_controller

--- app/javascript/controllers/flatpickr_controller.js
+++ app/javascript/controllers/flatpickr_controller.js
@@ -6,49 +6,25 @@ export default class extends Controller {
   static targets = ["start", "end"];
 
   connect() {
-    console.log("Hello flatpickr");
-    const startPicker = this.startTarget.flatpickr({
-      disable: JSON.parse(this.element.dataset.unavailable),
+    const unavailableDates = JSON.parse(this.element.dataset.unavailable);
+
+    flatpickr(this.startTarget, {
+      disable: unavailableDates,
       minDate: "today",
-      onChange: function (selectedDates, dateStr, instance) {
-        endPicker.set("minDate", dateStr);
+      onChange: function (selectedDates) {
+        let minDate = selectedDates[0];
+        minDate.setDate(minDate.getDate() + 1);
+        endPicker.set("minDate", minDate);
       },
     });
-    const endPicker = this.endTarget.flatpickr({
-      disable: JSON.parse(this.element.dataset.unavailable),
-      minDate: "today",
-      onChange: function (selectedDates, dateStr, instance) {
-        startPicker.set("maxDate", dateStr);
-      },
+
+    const tomorrow = new Date();
+    tomorrow.setDate(tomorrow.getDate() + 1);
+
+    const endPicker = flatpickr(this.endTarget, {
+      disable: unavailableDates,
+      dateFormat: "d-m-Y",
+      minDate: tomorrow,
     });
   }
 }
-// const startDateInput = document.getElementById("booking_start_date");
-// const endDateInput = document.getElementById("booking_end_date");
-
-//     if (startDateInput && endDateInput) {
-//       const unvailableDates = JSON.parse(
-//         document.querySelector(".widget-content").dataset.unavailable
-//       );
-
-//       flatpickr(startDateInput, {
-//         minDate: "today",
-//         dateFormat: "d-m-Y",
-//         disable: unvailableDates,
-//         onChange: function (selectedDates, selectedDate) {
-//           if (selectedDate === "") {
-//             endDateInput.disabled = true;
-//           }
-//           let minDate = selectedDates[0];
-//           minDate.setDate(minDate.getDate() + 1);
-//           endDateCalendar.set("minDate", minDate);
-//           endDateInput.disabled = false;
-//         },
-//       });
-//       const endDateCalendar = flatpickr(endDateInput, {
-//         dateFormat: "d-m-Y",
-//         disable: unvailableDates,
-//       });
-//     }
-//   }
-// }
