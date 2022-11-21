e7abdda52d5cd2a0cfc2f5f13eb07a4364ee3e5e
Olivia Bonnal <oliviabonnal@gmail.com>
2022-11-21 16:53:34 +0000

    added logic to display a map on flats show

--- app/javascript/controllers/map_controller.js
+++ app/javascript/controllers/map_controller.js
@@ -1,11 +1,12 @@
-import { Controller } from "@hotwired/stimulus"
+import { Controller } from "@hotwired/stimulus";
 
 // Connects to data-controller="map"
 export default class extends Controller {
   static values = {
     apiKey: String,
-    markers: Array
-  }
+    markers: Array,
+    page: String,
+  };
 
   connect() {
     // Authentify with MapBox API
@@ -13,7 +14,7 @@ export default class extends Controller {
     // Use MapBox in div
     this.map = new mapboxgl.Map({
       container: this.element,
-      style: "mapbox://styles/mapbox/streets-v10"
+      style: "mapbox://styles/mapbox/streets-v10",
     });
     this.#addMarkersToMap();
     this.#fitMapToMarkers();
@@ -22,28 +23,36 @@ export default class extends Controller {
   // Define a private method to add markers to the map
   #addMarkersToMap() {
     this.markersValue.forEach((marker) => {
-      const popup = new mapboxgl.Popup().setHTML(marker.info_window)
+      const popup = new mapboxgl.Popup().setHTML(marker.info_window);
 
       // Create HTML element for custom marker
-      const customMarker = document.createElement("div")
-      customMarker.className = "marker"
-      customMarker.style.backgroundImage = `url('${marker.image_url}')`
-      customMarker.style.backgroundSize = "contain"
-      customMarker.style.backgroundRepeat = "no-repeat"
-      customMarker.style.width = "35px"
-      customMarker.style.height = "35px"
+      const customMarker = document.createElement("div");
+      customMarker.className = "marker";
+      customMarker.style.backgroundImage = `url('${marker.image_url}')`;
+      customMarker.style.backgroundSize = "contain";
+      customMarker.style.backgroundRepeat = "no-repeat";
+      customMarker.style.width = "35px";
+      customMarker.style.height = "35px";
 
-      // Pas the custom element as an argument to the new marker
-      new mapboxgl.Marker(customMarker)
-      .setLngLat([marker.lng, marker.lat])
-      .setPopup(popup)
-      .addTo(this.map)
+      // Pass the custom element as an argument to the new marker
+      if (this.pageValue === "flats-index") {
+        new mapboxgl.Marker(customMarker)
+          .setLngLat([marker.lng, marker.lat])
+          .setPopup(popup)
+          .addTo(this.map);
+      } else if (this.pageValue === "flats-show") {
+        new mapboxgl.Marker(customMarker)
+          .setLngLat([marker.lng, marker.lat])
+          .addTo(this.map);
+      }
     });
   }
 
   #fitMapToMarkers() {
     const bounds = new mapboxgl.LngLatBounds();
-    this.markersValue.forEach(marker => bounds.extend([marker.lng, marker.lat]));
-    this.map.fitBounds(bounds, { padding: 70, maxZoom: 15, duration: 0 })
+    this.markersValue.forEach((marker) =>
+      bounds.extend([marker.lng, marker.lat])
+    );
+    this.map.fitBounds(bounds, { padding: 70, maxZoom: 15, duration: 0 });
   }
 }
