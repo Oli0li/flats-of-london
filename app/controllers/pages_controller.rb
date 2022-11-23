0f2470cd8cf2c2de2058d1cf6300af9066675c24
Olivia Bonnal <oliviabonnal@gmail.com>
2022-11-23 16:59:38 +0000

    styled tabs on the dashboard and added 'my properties' tab

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,5 +3,6 @@ class PagesController < ApplicationController
     @future_bookings = Booking.display_bookings("future", current_user)
     @past_bookings = Booking.display_bookings("past", current_user)
     @current_bookings = Booking.display_bookings("present", current_user)
+    @my_properties = current_user.flats
   end
 end
--- app/javascript/controllers/dashboard_controller.js
+++ app/javascript/controllers/dashboard_controller.js
@@ -2,27 +2,34 @@ import { Controller } from "@hotwired/stimulus";
 
 // Connects to data-controller="dashboard"
 export default class extends Controller {
-  static targets = ["booking", "details", "div"];
+  static targets = ["tab", "details", "div"];
   static values = {
     current: String,
   };
 
   connect() {
     const display = (event) => {
-      // Get the class of the button that was clicked (eg: "future-bookings active")
-      const buttonClass = event.path[0].className.split(" ");
+      // Get the tab that was clicked
+      const clickedTab = event.path[0];
+
+      // Set its class as "active", and the class of other tabs as "inactive"
+      clickedTab.classList.replace("inactive", "active");
+      this.tabTargets.forEach((tab) => {
+        if (tab !== clickedTab) tab.classList.replace("active", "inactive");
+      });
+
+      // Check if the first class of the tab that was clicked is also used in one of the details div
+      // If so, this details div is displayed, and the other ones are hidden
       this.detailsTargets.forEach((target) => {
-        // Check if the first class of the button that was clicked is also used in one of the details div
-        // If so, this details div is displayed, and the other ones are hidden
-        if (target.className.includes(buttonClass[0])) {
-          target.className = target.className.replace("d-none", "d-block");
+        if (target.className.includes(clickedTab.classList[0])) {
+          target.classList.replace("d-none", "d-block");
         } else {
           target.classList.replace("d-block", "d-none");
         }
       });
     };
 
-    this.bookingTargets.forEach((element) => {
+    this.tabTargets.forEach((element) => {
       element.addEventListener("click", display);
     });
   }
