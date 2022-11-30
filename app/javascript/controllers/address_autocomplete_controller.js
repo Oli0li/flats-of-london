da753a3c9008558e5db4c12111d5215555b9b7ac
Olivia Bonnal <oliviabonnal@gmail.com>
2022-11-30 16:14:29 +0000

    made it so that the full address gets autocompleted in the address input field on the new flat form

--- app/javascript/controllers/address_autocomplete_controller.js
+++ app/javascript/controllers/address_autocomplete_controller.js
@@ -14,40 +14,9 @@ export default class extends Controller {
       },
     });
 
-    autofill.addEventListener("retrieve", (event) => {
-      const featureCollection = event.detail;
-      console.log(featureCollection);
-      console.log(featureCollection.features[0].properties.full_address);
-      console.log(this.addressTarget);
-      this.addressTarget.setAttribute(
-        "value",
-        featureCollection.features[0].properties.full_address
-      );
-      console.log(`value : ${this.addressTarget.value}`);
-      console.log(`target: ${this.addressTarget}`);
+    autofill.addEventListener("retrieve", async (event) => {
+      const fullAddress = event.detail.features[0].properties.full_address;
+      this.addressTarget.value = await fullAddress;
     });
-
-    // console.log(this.addressTarget);
-    // this.addressTarget.addEventListener("keyup", () => {
-    //   const mapboxSearchListbox = document.getElementsByTagName(
-    //     "mapbox-search-listbox"
-    //   );
-    //   const seed = mapboxSearchListbox[0].dataset.seed;
-    //   console.log(mapboxSearchListbox);
-    //   console.log(seed);
-    //   const suggestions = document.getElementsByClassName(
-    //     `${seed}--Suggestion`
-    //   );
-    //   console.log(suggestions);
-    //   Array.from(suggestions).forEach((suggestion) => {
-    //     suggestion.addEventListener("click", () => {
-    //       const fullAddress = suggestion.getElementsByClassName(
-    //         `${seed}--SuggestionDesc`
-    //       );
-    //       console.log(fullAddress[0].innerHTML);
-    //       this.addressTarget.value = fullAddress[0].innerHTML;
-    //     });
-    //   });
-    // });
   }
 }
